635f2abd1485a38ee7af0a457fae373ceecda254
NikJensen97 <npjjensen@hotmail.com>
2023-04-19 20:45:05 -0700

    refactored DayList for value/onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,10 +6,11 @@ export default function DayList(props) {
   return (
     
       <DayListItem 
-        key={day.id}  
-        selected={day.name === props.day}
-        setDay={props.setDay} 
-        {...day} 
+      key={day.id} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.value}
+      setDay={props.onChange} 
       />
     
 
