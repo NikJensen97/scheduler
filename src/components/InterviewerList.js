ced599a5d9297c52545ca99a723530ee2623e4bc
NikJensen97 <npjjensen@hotmail.com>
2023-04-19 20:10:45 -0700

    refactored code

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,12 +6,13 @@ export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
     return (
       
-        <InterviewerListItem 
-          key={interviewer.id}  
-          selected={interviewer.id === props.interviewer}
-          setInterviewer={props.setInterviewer} 
-          {...interviewer} 
-        />
+      <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
+    />
       
   
     )
