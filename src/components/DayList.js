8fa39157d74a43dcb9199f89ece8ffe8df0905a1
NikJensen97 <npjjensen@hotmail.com>
2023-04-19 17:09:03 -0700

    implimented .map to function

---
+++ src/components/DayList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const schedule = props.days.map(day => {
+  return (
+    
+      <DayListItem 
+        key={day.id}  
+        selected={day.name === props.day}
+        setDay={props.setDay} 
+        {...day} 
+      />
+    
+
+  )
+  });
+  return (
+    <ul>
+      {schedule}
+    </ul>
+
+  )
+}
