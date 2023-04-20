c6d746956a97d8db5bf668569c1753733f9e5f09
NikJensen97 <npjjensen@hotmail.com>
2023-04-19 17:08:10 -0700

    refactored return statement. still causes an error with setDay

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,21 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
+  let daySpots = classNames({
+    "no spots remaining": props.spots === 0,
+    "1 spot remaining": props.spots === 1,
+
+  });
+  if (props.spots > 1) {
+    daySpots = `${props.spots} spots remaining`
+  }
+
   return (
     <li className={dayClass}
-    onClick={() => props.setDay(props.name)}>
+      onClick={() => props.setDay(props.name)}
+      data-testid='day'>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{daySpots}</h3>
     </li>
   );
 }
