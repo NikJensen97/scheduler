7131e1b3a2a0118ebbc88903cf45e00c9233e188
NikJensen97 <npjjensen@hotmail.com>
2023-04-24 23:07:31 -0700

    imports visualmode + decides whether to show empty or show

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,35 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
-
-
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <>
       <Header
-      time={props.time} />
+        time={props.time} />
       <article className="appointment"></article>
-      {props.interview ? <Show interviewer={props.interview.interviewer.name} student={props.interview.student} /> : <Empty/>}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+      <Form
+      onCancel={() => back()}
+      interviewers={props.interviewers}
+      
+      />)}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      )}
     </>
   )
 
