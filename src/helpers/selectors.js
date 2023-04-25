1bbc38d44b464d622499865c059ac40dfccb3dc4
NikJensen97 <npjjensen@hotmail.com>
2023-04-24 23:07:59 -0700

    added get interviewsforday helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,6 +7,16 @@ export function getAppointmentsForDay(state, day) {
   return appointments;
 };
 
+
+export function getInterviewersForDay(state, day) {
+  const selectedDay = state.days.find(dayBooked => dayBooked.name === day);
+  if (!selectedDay) {
+    return [];
+  }
+  const interviewers = selectedDay.interviewers.map(id => state.interviewers[id]);
+  return interviewers;
+};
+
 export function getInterview(state, interview) {
   if (interview === null) {
     return null;
@@ -23,4 +33,3 @@ export function getInterview(state, interview) {
   return theInterview;
 
 };
-
