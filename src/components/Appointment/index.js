1950e8c5301c99e0669aa4a26ade963841bb9bf1
NikJensen97 <npjjensen@hotmail.com>
2023-04-25 21:06:52 -0700

    save function passed to form. now displays SHOW and STATUS

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,15 +5,27 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = 'CREATE';
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview).then(
+
+    transition(SHOW));
+  }
   return (
     <>
       <Header
@@ -21,17 +33,19 @@ export default function Appointment(props) {
       <article className="appointment"></article>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && (
-      <Form
-      onCancel={() => back()}
-      interviewers={props.interviewers}
-      
-      />)}
+        <Form
+          onCancel={() => back()}
+          interviewers={props.interviewers}
+          onSave={save}
+
+        />)}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
         />
       )}
+      {mode === SAVING && <Status message={"Saving..."} />}
     </>
   )
 
