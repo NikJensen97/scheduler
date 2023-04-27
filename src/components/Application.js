3fae0db4d2a3f12ab57ea83b6258ffea2d54550f
NikJensen97 <npjjensen@hotmail.com>
2023-04-26 19:01:36 -0700

    data moved over to a hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,68 +6,19 @@ import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 import { getInterview } from "helpers/selectors";
 import { getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
+
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+
   let dailyAppointments = [];
   let dailyInterviews = [];
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
-  useEffect(() => {
-
-    Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({
-        ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data
-      }));
-    })
-    // console.log(state.interviewers)
-  }, []);
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-
-    axios.put(`http://localhost:8001/api/appointments/${id}`, {
-      interview
-    }
-    ).then(setState({
-      ...state,
-      appointments
-    }))
-    return Promise.resolve();
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id]
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    axios.delete(`http://localhost:8001/api/appointments/${id}`).then(
-      setState({
-        ...state,
-        appointments
-      }))
-      return Promise.resolve();
-  }
-
   dailyInterviews = getInterviewersForDay(state, state.day);
 
   dailyAppointments = getAppointmentsForDay(state, state.day);
