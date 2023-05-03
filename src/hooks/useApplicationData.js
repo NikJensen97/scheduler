71354012ac35f2572475bf804174409bbc25b993
NikJensen97 <npjjensen@hotmail.com>
2023-05-02 18:12:10 -0700

    added anonymous function to cancel interview to fix crash

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -31,9 +31,9 @@ export default function useApplicationData() {
   useEffect(() => {
 
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({
         ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data
@@ -52,7 +52,7 @@ export default function useApplicationData() {
     };
 
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {
+    return axios.put(`/api/appointments/${id}`, {
       interview
     }
     ).then(setState({
@@ -71,12 +71,13 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`).then(
+    return axios.delete(`/api/appointments/${id}`).then(() => {
       setState({
         ...state,
         appointments,
         days: updateSpots(state, appointments)
-      }))
+      })
+    })
   }
   return {
     state: state,
